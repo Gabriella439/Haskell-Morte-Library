7101d3b280f82e38006d4653de40b148d57ab14e
Gabriel Gonzalez <Gabriel439@gmail.com>
2014-09-06 17:42:02 -0700

    Changed `morte` executable to use `stdin`/`stderr`/`stdout`

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,31 +1,26 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
-import Data.Monoid ((<>))
 import qualified Data.Text.Lazy.IO as Text
-import Morte.Core
-import Morte.Parser
+import Morte.Core (typeOf, prettyTypeError, prettyExpr, normalize)
+import Morte.Parser (exprFromText, prettyParseError)
 import Options.Applicative
-
-options :: Parser (FilePath, FilePath)
-options =
-        (,)
-    <$> argument str (metavar "INFILE")
-    <*> argument str (metavar "OUTFILE")
+import System.IO (stderr)
 
 main :: IO ()
 main = do
-    (inFile, outFile) <- execParser $ info (helper <*> options)
+    execParser $ info (helper <*> pure ())
         (   fullDesc
         <>  header "morte - A bare-bones calculus of constructions"
-        <>  progDesc "Type-check and normalize a Morte program"
+        <>  progDesc "Type-check and normalize a Morte program, reading the \
+                     \program from standard input, writing the program's type \
+                     \to standard error, and writing the normalized program to\
+                     \standard output"
         )
-    inText <- Text.readFile inFile
+    inText <- Text.getContents
     case exprFromText inText of
-        Left  pe   -> Text.putStr (prettyParseError pe)
+        Left  pe   -> Text.hPutStr stderr (prettyParseError pe)
         Right expr -> case typeOf expr of
-            Left  te       -> Text.putStr (prettyTypeError te)
+            Left  te       -> Text.hPutStr stderr (prettyTypeError te)
             Right typeExpr -> do
-                Text.putStrLn (prettyExpr (normalize typeExpr))
-                Text.writeFile outFile (prettyExpr (normalize expr) <> "\n")
+                Text.hPutStrLn stderr (prettyExpr (normalize typeExpr))
+                Text.putStrLn (prettyExpr (normalize expr))
