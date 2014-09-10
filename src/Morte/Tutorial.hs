f0a5134a803b06633cef3053e55004923af1f9d5
Gabriel Gonzalez <Gabriel439@gmail.com>
2014-09-09 17:32:44 -0700

    Updated documentation

--- src/Morte/Tutorial.hs
+++ src/Morte/Tutorial.hs
@@ -1500,6 +1500,8 @@ the generating step function:
     strictly necessary, but doing so makes Morte aware of the monad laws, which
     allows it to greatly simplify the program:
 
+> -- recursive.mt
+>
 > -- The Haskell code we will translate to Morte:
 > --
 > --     import Prelude hiding (
@@ -1803,10 +1805,180 @@ input to standard output:
     which allows the normalizer to optimize away monadic abstractions like
     @replicateM_@.
 
-    As an exercise, try implementing a corecursive syntax tree instead of a
-    recursive one, perhaps one that loops infinitely.  For such a syntax tree
-    the backend language could then step the corecursive syntax tree
-    indefinitely.
+    You can also build corecursive programs with effects.  Here is an example of
+    a corecursive @IO@ syntax tree and a program that infinitely echoes
+    standard input to standard output:
+
+> -- corecursive.mt
+>
+> -- data IOF r s = PutStrLn String s | GetLine (String -> s) | Return r
+> --
+> -- data IO r = forall s . MkIO s (s -> IOF r s)
+> --
+> -- main = MkIO Nothing (maybe (\str -> PutStrLn str Nothing) (GetLine Just))
+> 
+> (   \(String : *)
+> ->  (   \(Maybe : * -> *)
+>     ->  \(Just : forall (a : *) -> a -> Maybe a)
+>     ->  \(Nothing : forall (a : *) -> Maybe a)
+>     ->  \(  maybe
+>         :   forall (a : *) -> Maybe a -> forall (x : *) -> (a -> x) -> x -> x
+>         )
+>     ->  \(IOF : * -> * -> *)
+>     ->  \(  PutStrLn
+>         :   forall (r : *)
+>         ->  forall (s : *)
+>         ->  String
+>         ->  s
+>         ->  IOF r s
+>         )
+>     ->  \(  GetLine
+>         :   forall (r : *)
+>         ->  forall (s : *)
+>         ->  (String -> s)
+>         ->  IOF r s
+>         )
+>     ->  \(  Return
+>         :   forall (r : *)
+>         ->  forall (s : *)
+>         ->  r
+>         ->  IOF r s
+>         )
+>     ->  (   \(IO : * -> *)
+>         ->  \(  MkIO
+>             :   forall (r : *) -> forall (s : *) -> s -> (s -> IOF r s) -> IO r
+>             )
+>         ->  (   \(main : forall (r : *) -> IO r)
+>             ->  main
+>             )
+> 
+>             -- main
+>             (   \(r : *)
+>             ->  MkIO
+>                 r
+>                 (Maybe String)
+>                 (Nothing String)
+>                 (   \(m : Maybe String)
+>                 ->  maybe
+>                         String
+>                         m
+>                         (IOF r (Maybe String))
+>                         (\(str : String) ->
+>                             PutStrLn r (Maybe String) str (Nothing String)
+>                         )
+>                         (GetLine r (Maybe String) (Just String))
+>                 )
+>             )
+>         )
+> 
+>         -- IO
+>         (   \(r : *)
+>         ->  forall (x : *)
+>         ->  (forall (s : *) -> s -> (s -> IOF r s) -> x)
+>         ->  x
+>         )
+> 
+>         -- MkIO
+>         (   \(r : *)
+>         ->  \(s : *)
+>         ->  \(seed : s)
+>         ->  \(step : s -> IOF r s)
+>         ->  \(x : *)
+>         ->  \(k : forall (s : *) -> s -> (s -> IOF r s) -> x)
+>         ->  k s seed step
+>         )
+>     )
+> 
+>     -- Maybe
+>     (\(a : *) -> forall (x : *) -> (a -> x) -> x -> x)
+> 
+>     -- Just
+>     (   \(a : *)
+>     ->  \(va : a)
+>     ->  \(x : *)
+>     ->  \(Just : a -> x)
+>     ->  \(Nothing : x)
+>     ->  Just va
+>     )
+> 
+>     -- Nothing
+>     (   \(a : *)
+>     ->  \(x : *)
+>     ->  \(Just : a -> x)
+>     ->  \(Nothing : x)
+>     ->  Nothing
+>     )
+> 
+>     -- maybe
+>     (\(a : *) -> \(m : forall (x : *) -> (a -> x) -> x -> x) -> m)
+> 
+>     -- IOF
+>     (   \(r : *)
+>     ->  \(s : *)
+>     ->  forall (x : *)
+>     ->  (String -> s -> x)
+>     ->  ((String -> s) -> x)
+>     ->  (r -> x)
+>     ->  x
+>     )
+> 
+>     -- PutStrLn
+>     (   \(r : *)
+>     ->  \(s : *)
+>     ->  \(str : String)
+>     ->  \(vs : s)
+>     ->  \(x : *)
+>     ->  \(PutStrLn : String -> s -> x)
+>     ->  \(GetLine : (String -> s) -> x)
+>     ->  \(Return : r -> x)
+>     ->  PutStrLn str vs
+>     )
+> 
+>     -- GetLine
+>     (   \(r : *)
+>     ->  \(s : *)
+>     ->  \(k : String -> s)
+>     ->  \(x : *)
+>     ->  \(PutStrLn : String -> s -> x)
+>     ->  \(GetLine : (String -> s) -> x)
+>     ->  \(Return : r -> x)
+>     ->  GetLine k
+>     )
+> 
+>     -- Return
+>     (   \(r : *)
+>     ->  \(s : *)
+>     ->  \(vr : r)
+>     ->  \(x : *)
+>     ->  \(PutStrLn : String -> s -> x)
+>     ->  \(GetLine : (String -> s) -> x)
+>     ->  \(Return : r -> x)
+>     ->  Return vr
+>     )
+> 
+> )
+
+    If you compile this corecursive program you will get a state machine which
+    can then be passed to a backend to step the state machine indefinitely:
+
+> $ morte < corecursive.mt
+> ∀(String : *) → ∀(r : *) → ∀(x : *) → (∀(s : *) → s → (s → ∀(x : *) → (String 
+> → s → x) → ((String → s) → x) → (r → x) → x) → x) → x
+> λ(String : *) → λ(r : *) → λ(x : *) → λ(k : ∀(s : *) → s → (s → ∀(x : *) → (St
+> ring → s → x) → ((String → s) → x) → (r → x) → x) → x) → k (∀(x : *) → (String
+>  → x) → x → x) (λ(x : *) → λ(Just : String → x) → λ(Nothing : x) → Nothing) (λ
+> (m : ∀(x : *) → (String → x) → x → x) → m (∀(x : *) → (String → (∀(x : *) → (S
+> tring → x) → x → x) → x) → ((String → ∀(x : *) → (String → x) → x → x) → x) → 
+> (r → x) → x) (λ(str : String) → λ(x : *) → λ(PutStrLn : String → (∀(x : *) → (
+> String → x) → x → x) → x) → λ(GetLine : (String → ∀(x : *) → (String → x) → x 
+> → x) → x) → λ(Return : r → x) → PutStrLn str (λ(x : *) → λ(Just : String → x) 
+> → λ(Nothing : x) → Nothing)) (λ(x : *) → λ(PutStrLn : String → (∀(x : *) → (St
+> ring → x) → x → x) → x) → λ(GetLine : (String → ∀(x : *) → (String → x) → x → 
+> x) → x) → λ(Return : r → x) → GetLine (λ(va : String) → λ(x : *) → λ(Just : St
+> ring → x) → λ(Nothing : x) → Just va)))
+
+    Any manipulations of this corecursive syntax tree within Morte will compile
+    to efficient state transitions.
 -}
 
 {- $portability
