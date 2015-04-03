f791c1c0934aae5ec8cacbb3f8547bb9c1f2e545
Gabriel Gonzalez <Gabriel439@gmail.com>
2015-04-02 20:21:35 -0700

    Changed pretty-printing of `forall`-bound variables
    Now the rule is that `_` is not printed, but any other variable is.  The
    rationale behind this is that I would like pretty-printing to preserve variable
    names.

--- src/Morte/Core.hs
+++ src/Morte/Core.hs
@@ -368,7 +368,7 @@ buildExpr = go False False
             <>  (if parenBind then ")" else "")
         Pi  x _A b ->
                 (if parenBind then "(" else "")
-            <>  (if used x b
+            <>  (if x /= "_"
                  then
                      "∀(" <> fromLazyText x <> " : " <> go False False _A <> ")"
                  else go True False _A )
