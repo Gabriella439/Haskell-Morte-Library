b92e5e80d974d14631cd89578c8e0b228e3b0c59
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-06-11 15:50:44 -0700

    Add Nix derivation to build package

---
+++ default.nix
@@ -0,0 +1,29 @@
+{ mkDerivation, alex, array, base, binary, code-page, containers
+, criterion, deepseq, Earley, http-client, http-client-tls
+, microlens, microlens-mtl, mtl, optparse-applicative, pipes
+, QuickCheck, stdenv, system-fileio, system-filepath, tasty
+, tasty-hunit, tasty-quickcheck, text, text-format, transformers
+}:
+mkDerivation {
+  pname = "morte";
+  version = "1.6.7";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    array base binary containers deepseq Earley http-client
+    http-client-tls microlens microlens-mtl pipes system-fileio
+    system-filepath text text-format transformers
+  ];
+  libraryToolDepends = [ alex ];
+  executableHaskellDepends = [
+    base code-page optparse-applicative text text-format
+  ];
+  testHaskellDepends = [
+    base mtl QuickCheck system-filepath tasty tasty-hunit
+    tasty-quickcheck text transformers
+  ];
+  benchmarkHaskellDepends = [ base criterion system-filepath text ];
+  description = "A bare-bones calculus of constructions";
+  license = stdenv.lib.licenses.bsd3;
+}
---
+++ release.nix
@@ -0,0 +1,27 @@
+# You can build this repository using Nix by running:
+#
+#     $ nix-build -A morte release.nix
+#
+# You can also open up this repository inside of a Nix shell by running:
+#
+#     $ nix-shell -A morte.env release.nix
+#
+# ... and then Nix will supply the correct Haskell development environment for
+# you
+let
+  config = {
+    packageOverrides = pkgs: {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: {
+          morte = haskellPackagesNew.callPackage ./default.nix { };
+        };
+      };
+    };
+  };
+
+  pkgs =
+    import <nixpkgs> { inherit config; };
+
+in
+  { morte = pkgs.haskellPackages.morte;
+  }
