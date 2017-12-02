c0edbddc25231d84e753b0552ea0b7e2571fd135
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-12-01 18:22:51 -0800

    Version 1.6.11 → 1.6.12

--- default.nix
+++ default.nix
@@ -1,15 +1,16 @@
 { mkDerivation, alex, array, base, binary, code-page, containers
-, deepseq, Earley, http-client, http-client-tls, microlens
-, microlens-mtl, mtl, optparse-applicative, pipes, QuickCheck
-, stdenv, system-fileio, system-filepath, tasty, tasty-hunit
-, tasty-quickcheck, text, text-format, transformers
+, criterion, deepseq, Earley, http-client, http-client-tls
+, microlens, microlens-mtl, mtl, optparse-applicative, pipes
+, QuickCheck, stdenv, system-fileio, system-filepath, tasty
+, tasty-hunit, tasty-quickcheck, text, text-format, transformers
 }:
 mkDerivation {
   pname = "morte";
-  version = "1.6.11";
+  version = "1.6.12";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
+  enableSeparateDataOutput = true;
   libraryHaskellDepends = [
     array base binary containers deepseq Earley http-client
     http-client-tls microlens microlens-mtl pipes system-fileio
@@ -23,6 +24,7 @@ mkDerivation {
     base mtl QuickCheck system-filepath tasty tasty-hunit
     tasty-quickcheck text transformers
   ];
+  benchmarkHaskellDepends = [ base criterion system-filepath text ];
   description = "A bare-bones calculus of constructions";
   license = stdenv.lib.licenses.bsd3;
 }
